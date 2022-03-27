99fec6d1eaf3f01ea859223e9e61425b15f1a7ba
Daniel Williams <danielpaulwill@gmail.com>
2022-03-26 20:38:14 -0400

    created side items list

---
+++ src/RightSidebar.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import ItemsList from "./ItemsList"
+
+function RightSidebar({ currentItems }) {
+
+  const items = currentItems.map(item => (
+    <ItemsList item={item}/>
+    ))
+
+  return (
+    <div id="rightSidebar">
+      <div className="sidebarStats">
+        <h3>Items</h3>
+        {items}
+      </div>
+    </div>
+  )
+};
+
+export default RightSidebar;
